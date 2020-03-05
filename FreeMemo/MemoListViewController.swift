0fcecc7a63592e182b37ef3ac714990c0972d013
EZDev-sh <ezdev.sh@gmail.com>
2020-03-05 17:01:18 +0900

    iOS 10.0설정, 스토리보드 삭제 및 설정

---
+++ FreeMemo/MemoListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MemoListViewController.swift
+//  FreeMemo
+//
+//  Created by EZDev on 2020/03/05.
+//  Copyright © 2020 EZDev. All rights reserved.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationItem.title = "모든메모"
+        
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
