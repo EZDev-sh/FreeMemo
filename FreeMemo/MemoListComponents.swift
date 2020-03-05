5cc833062f69943b46298833b0722e4af59b07bf
EZDev-sh <ezdev.sh@gmail.com>
2020-03-05 16:59:21 +0900

    iOS 10.0설정, 스토리보드 삭제 및 설정

---
+++ FreeMemo/MemoListComponents.swift
@@ -0,0 +1,13 @@
+//
+//  MemoListComponents.swift
+//  FreeMemo
+//
+//  Created by EZDev on 2020/03/05.
+//  Copyright © 2020 EZDev. All rights reserved.
+//
+
+import UIKit
+
+class Component {
+    
+}
