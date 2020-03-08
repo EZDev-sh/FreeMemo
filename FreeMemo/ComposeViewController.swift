db8cbdae759146b365f3196653ae06eb16c1e6f4
EZDev-sh <ezdev.sh@gmail.com>
2020-03-08 18:19:35 +0900

    사진, 카메라 사용 하여 이미지 추가

--- FreeMemo/ComposeViewController.swift
+++ FreeMemo/ComposeViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import Photos
 
 class ComposeViewController: UIViewController {
     
     var isKeyboard: Bool = true
+    let picker: UIImagePickerController = UIImagePickerController()
+    var addImages: [UIImage] = []
+    var addName: [String?] = []
     
     // 네비게이션바에 필요한 버튼 정의
     // create by EZDev on 2020.03.07
@@ -113,18 +117,21 @@ class ComposeViewController: UIViewController {
         // 키보드의 높이를 가져올수 있게 하는 노티를 등록한다
         // create by EZDev on 2020.03.08
         NotificationCenter.default.addObserver(self, selector: #selector(showKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        view.endEditing(true)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // textview 델리게이트, tableview datasource 등록
-        // create by EZDev on 2020.03.07
+        // textview 델리게이트, tableview datasource, delegat, cell 등록
+        // create by EZDev on 2020.03.08
         contentTextView.delegate = self
         imageTableView.delegate = self
         imageTableView.dataSource = self
         imageTableView.register(PictureCell.self, forCellReuseIdentifier: "pictureCell")
+        picker.delegate = self
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -162,9 +169,7 @@ class ComposeViewController: UIViewController {
             imageTableView.bottomAnchor.constraint(equalTo: guide.bottomAnchor),
             imageTableView.leadingAnchor.constraint(equalTo: editToolBar.leadingAnchor),
             imageTableView.trailingAnchor.constraint(equalTo: editToolBar.trailingAnchor),
-            
-            
-            
+  
         ])
     }
     
@@ -179,8 +184,15 @@ class ComposeViewController: UIViewController {
         // core data에 저장하는 코드 구현 예정
     }
     
+    // 사진을 추가하는 방식의 액션시트를 보여줍니다.
+    // create by EZDev on 2020.03.08
     @objc func picture(_ sender: Any) {
-        // 사진 추가되는거 구현 예정
+        if isKeyboard {
+            isKeyboard = false
+            view.endEditing(true)
+            keyboardBtn.image = UIImage(named: "show_keyboard")
+        }
+        showAlert()
     }
     
     // 키보드 버튼이 눌렸을때 키보드 on/off 기능
@@ -206,9 +218,33 @@ class ComposeViewController: UIViewController {
             imageTableView.heightAnchor.constraint(equalToConstant: height).isActive = true
         }
     }
+    
+    // 이미지 추가방식의 액션시트를 정의 합니다.
+    // create by EZDev on 2020.03.08
+    func showAlert() {
+        let alert = UIAlertController(title: "이미지 추가방식을 선책해주세요.", message: .none, preferredStyle: .actionSheet)
+        let cam = UIAlertAction(title: "카메라", style: .default) { (action) in
+            // 카메라에 접근
+            self.openCam()
+        }
+        let album = UIAlertAction(title: "앨범", style: .default) { (action) in
+            // 앨범에 접근
+            self.openAlbum()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        
+        alert.addAction(cam)
+        alert.addAction(album)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
+// textview에 placeholde 기능을 구현
+// create by EZDev on 2020.03.05
 extension ComposeViewController: UITextViewDelegate {
     func textViewDidBeginEditing(_ textView: UITextView) {
         initTextview()
@@ -230,27 +266,84 @@ extension ComposeViewController: UITextViewDelegate {
     }
 }
 
+
+// 추가된 이미지가 보여지는 테이블뷰의 데이터, 델리게이트 정의
+// create by EZDev on 2020.03.08
 extension ComposeViewController: UITableViewDataSource, UITableViewDelegate {
+    // 테이블에 표현할 셀의 갯수
+    // create by EZDev on 2020.03.08
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return addImages.count
     }
     
+    // 셀에 보여질 아이템 업데이트
+    // create by EZDev on 2020.03.08
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "pictureCell", for: indexPath) as! PictureCell
         
-        cell.thumbnail.image = UIImage(named: "Splash_image")
-        cell.imageName.text = "EZDeveloper"
+        cell.thumbnail.image = addImages[indexPath.row]
+        cell.imageName.text = addName[indexPath.row]
         
         return cell
     }
     
+    // 추가된 이미지셀을 삭제
+    // create by EZDev on 2020.03.08
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let delete = UITableViewRowAction(style: .destructive, title: "삭제") { (action, indexPath) in
-            print(indexPath.row)
+            self.addImages.remove(at: indexPath.row)
+            self.addName.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            print(self.addName)
         }
-        
         return [delete]
     }
+
+}
+
+// 앨범, 카메라 접근 컨트롤
+// create by EZDev on 2020.03.08
+extension ComposeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // 앨범 접근후 화면에 보여주기
+    // create by EZDev on 2020.03.08
+    func openAlbum() {
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true, completion: nil)
+    }
     
+    // 카메라 접근후 화면에 보여주기
+    // creat by EZDev on 2020.03.08
+    func openCam() {
+        picker.sourceType = .camera
+        
+        present(picker, animated: true, completion: nil)
+    }
     
+    // 앨범에서 선택한 이미지와 이미지 이름을 배열에 저장한다.
+    // creat by EZDev on 2020.03.08
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            addImages.append(image)
+        }
+        if let url = info[UIImagePickerController.InfoKey.referenceURL] as? URL {
+            let asset = PHAsset.fetchAssets(withALAssetURLs: [url], options: nil)
+            guard let firstObj = asset.firstObject else { return }
+            let fileName = PHAssetResource.assetResources(for: firstObj).first?.originalFilename
+            
+            addName.append(fileName)
+        }
+        else {
+            // 카메라 접근시 날짜로 이름을 설정후 배열에 저장한다.
+            let date = Date()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            addName.append(dateFormatter.string(from: date))
+        }
+        
+        imageTableView.reloadData()
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
