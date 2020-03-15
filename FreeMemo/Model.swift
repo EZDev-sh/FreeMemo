0bac295c057d5789ce103cf60eba384657d26f0f
EZDev-sh <ezdev.sh@gmail.com>
2020-03-15 18:16:58 +0900

    ignore에 pod 추가, 프로젝트에 RealmSwift추가

--- FreeMemo/Model.swift
+++ FreeMemo/Model.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 EZDev. All rights reserved.
 //
 
+// 기존 데이터 모델
 import Foundation
 import UIKit
 
@@ -23,3 +24,20 @@ class DataMgr {
     
     var memoList = [Model]()
 }
+
+// RealmSwift를 사용한 데이터 모델
+// create by EZDev on 2020.03.15
+//
+//import RealmSwift
+//
+//class Memo: Object {
+//    @objc dynamic var title: String = ""
+//    @objc dynamic var content: String = ""
+//    
+//    let imageArray: List<Picture> = List<Picture>()
+//}
+//
+//class Picture: Object {
+//    @objc dynamic var name: String = ""
+//    @objc dynamic var imageData: Data = Data()
+//}
